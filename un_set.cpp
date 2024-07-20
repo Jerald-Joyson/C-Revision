5053d5e10177d7c7f237a8e0bd73cb05cb339b5c
Jerald-Joyson <jeraldjoyson.inc@gmail.com>
2024-07-20 09:28:16 +0530

    add new file

---
+++ un_set.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+int main(){
+	int a=99;
+	unordered_set<int> ch = {1,2,3,4,5};
+while(a!=0){
+	cout<<"enter no";
+	cin>>a;
+	if(ch.find(a) != ch.end()){
+		cout<<"element already exist"<<endl;
+	}else{
+		ch.insert(a);
+		cout<<"inserted"<<endl;
+	}
+
+	for(unordered_set<int>::iterator it = ch.begin(); it != ch.end();it++){
+		cout<<*it<<endl;
+	}
+}
+
+	return 0;
+}
+
