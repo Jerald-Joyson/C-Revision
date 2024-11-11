b13ef7a96ea3ffaa2190c5d3ca6eeef48ac4bb6d
Jerald Joyson <jeraldjoyson.inc@gmail.com>
2024-11-11 23:35:06 +0530

    add 11th day program

--- leetcode-problems/2601_Prime_Subtraction_Operation.cpp
+++ leetcode-problems/2601_Prime_Subtraction_Operation.cpp
@@ -10,17 +10,53 @@
 // In the second operation: i = 1, p = 7, subtract 7 from nums[1], so nums becomes equal to [1,2,6,10].
 // After the second operation, nums is sorted in strictly increasing order, so the answer is true.
 
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
+    void sieveOfEratosthenes(vector<int>& primes, int maxVal) {
+        bool sieve[maxVal + 1];
+        memset(sieve, false, sizeof(sieve));
+        for (int i = 2; i * i < maxVal; ++i) {
+            if (sieve[i] == false) {
+                for (int j = 2; i * j < maxVal; ++j)
+                    sieve[i * j] = true;
+            }
+        }
+        for (int i = 2; i < maxVal; ++i)
+            if (sieve[i] == false)
+                primes.push_back(i);
+    }
+
 public:
     bool primeSubOperation(vector<int>& nums) {
-        
-        
+        vector<int> primes;
+        int maxVal = *max_element(nums.begin(), nums.end());
+        sieveOfEratosthenes(primes, maxVal);
+
+        // Greedy: Try minimizing each value
+        for (int i = 0; i < nums.size(); ++i) {
+            int diff = i == 0 ? nums[0] - 1 : nums[i] - nums[i - 1] - 1;
+            int lb = lower_bound(primes.begin(), primes.end(), diff) -
+                     primes.begin();
+
+            if (lb == primes.size() or primes[lb] > diff)
+                lb--;
+            if (lb < 0) {
+                if (i == 0 or nums[i] > nums[i - 1])
+                    continue;
+                else
+                    return false;
+            }
+            nums[i] -= primes[lb];
+        }
+        return true;
     }
 };
 
 int main(){
     Solution obj;
     vector<int> nums = {4,9,6,10};
-    cout << obj.primeSubOperation(nums) << endl;
+    cout << (obj.primeSubOperation(nums) ? "true" : "false") << endl;
     return 0;
 }
